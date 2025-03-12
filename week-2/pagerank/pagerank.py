8d50056d098134d38ac9fb91f962b06d3afea3e8
panprogramadorgh <barreroalvaro2007@gmail.com>
2025-03-12 21:55:53 +0100

    transition_model; sample_pagerank; iterate_pagerank

--- week-2/pagerank/pagerank.py
+++ week-2/pagerank/pagerank.py
@@ -40,15 +40,17 @@ def crawl(directory):
 
     # Only include links to other pages in the corpus
     for filename in pages:
-        pages[filename] = set(
-            link for link in pages[filename]
-            if link in pages
-        )
+        pages[filename] = set(link for link in pages[filename] if link in pages)
 
     return pages
 
 
-def transition_model(corpus, page, damping_factor):
+def corpus_key(corpus: dict[str, set], i: int):
+    """Returns an specific corpus page by its index"""
+    return tuple(corpus.keys())[i]
+
+
+def transition_model(corpus: dict[str, set], page: str, damping_factor: float):
     """
     Return a probability distribution over which page to visit next,
     given a current page.
@@ -57,10 +59,23 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+
+    # Checks whether other pages links this page
+    page_is_linked = False
+    for links in corpus.values():
+        if page in links:
+            page_is_linked = True
+            break
+
+    # If the page is not linked or damping factor is over the threshold then a return a random page from the entire corpus
+    if random.random() > damping_factor or not page_is_linked:
+        return corpus_key(corpus, random.randint(0, len(corpus) - 1))
+
+    # We return a random page linked from the current page
+    return list(corpus[page])[random.randint(0, len(corpus[page]) - 1)]
 
 
-def sample_pagerank(corpus, damping_factor, n):
+def sample_pagerank(corpus: dict[str, set], damping_factor: float, n: int):
     """
     Return PageRank values for each page by sampling `n` pages
     according to transition model, starting with a page at random.
@@ -69,10 +84,26 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+
+    remaining_samples = n
+    current_page = corpus_key(corpus, random.randint(0, len(corpus) - 1))
+
+    # Initialize the dictionary to store the page rank values
+    pagerank = {}
+    for page in corpus.keys():
+        pagerank[page] = 0
+
+    while remaining_samples:
+        remaining_samples -= 1
+
+        # Calculates next page from current page
+        current_page = transition_model(corpus, current_page, damping_factor)
+        pagerank[current_page] += 1 / n
+
+    return pagerank
 
 
-def iterate_pagerank(corpus, damping_factor):
+def iterate_pagerank(corpus: dict[str, set], damping_factor: float):
     """
     Return PageRank values for each page by iteratively updating
     PageRank values until convergence.
@@ -81,8 +112,33 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
 
+    pagerank = {}
+
+    # Initialize the dictionary to store the page rank values
+    for page in corpus.keys():
+        pagerank[page] = 1 / len(corpus)
+
+    def probability(page: str):
+        return ((1 - damping_factor) / len(corpus)) + damping_factor * sum(
+            set(
+                # Current page probability / number of links
+                pagerank[i] / len(corpus[i])
+                for i in corpus.keys()
+                if page in corpus[i]
+            )
+        )
+    
+    # Previous probability for first page
+    prev = 0
+
+    # We balance each page probability until convergence
+    while abs(prev - pagerank[corpus_key(corpus, 0)]) > 0.001:
+        prev = pagerank[corpus_key(corpus, 0)]
+        for page in corpus:
+            pagerank[page] = probability(page)
+
+    return pagerank
 
 if __name__ == "__main__":
     main()
